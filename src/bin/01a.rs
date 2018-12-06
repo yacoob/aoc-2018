214b711b1655ce650e0d95c251a4406e9221de40
Jakub Turski <yacoob@gmail.com>
2018-12-06 22:08:45 +0000

    Further improvements to 01a.rs

--- src/bin/01a.rs
+++ src/bin/01a.rs
@@ -1,22 +1,12 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::prelude::*;
 
 fn main() {
-    // Read puzzle input. Aren't we optimistic that everything will work just fine? :D
-    let f = File::open("inputs/01").unwrap();
-    let buffered = BufReader::new(&f);
-    
-    // You can substantially simplify all of this with iterator adapters:
-    let freq = buffered.lines().map(|line| line.unwrap().parse::<i32>().unwrap()).sum();
-    println!("Final frequency: {}", freq);
-    
-    // Also, generally BufRead::lines is a bit of an antipattern, because it does way
-    // too much allocation (because it creates a new string for every line). This is a bit
-    // more idiomatic:
     let mut input = String::new();
-    let f = File::open("inputs/01").unwrap();
-    f.read_to_string(&mut input).unwrap();
-    let freq = input.lines().map(|line| line.parse::<i32>().unwrap()).sum();
+    // Read puzzle input. Aren't we optimistic that everything will work just fine? :D
+    let mut file = File::open("inputs/01").unwrap();
+    file.read_to_string(&mut input).unwrap();
+    // Look, ma! Fully functional!
+    let freq: i32 = input.lines().map(|line| line.parse::<i32>().unwrap()).sum();
     println!("Final frequency: {}", freq);
-    
 }
