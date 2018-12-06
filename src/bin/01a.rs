e6fc4e29552140df9f707b90eb46d19dddccc909
Nathan West <Lucretiel@users.noreply.github.com>
2018-12-06 11:28:57 -0800

    Update 01a.rs

--- src/bin/01a.rs
+++ src/bin/01a.rs
@@ -1,23 +1,22 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    use std::fs::File;
-    use std::io::{BufRead, BufReader};
-
-    let starting_frequency = 0;
-    let mut current_frequency = starting_frequency;
-
     // Read puzzle input. Aren't we optimistic that everything will work just fine? :D
-    let mut changes: Vec<String> = Vec::new();
     let f = File::open("inputs/01").unwrap();
     let buffered = BufReader::new(&f);
-    for line in buffered.lines() {
-        changes.push(line.unwrap());
-    }
-
-    // Iterate through frequency changes:
-    //  part A: find the frequency after applying entire list of adjustments;
-    for change in changes {
-        let delta: i32 = change.parse().unwrap();
-        current_frequency += delta;
-    }
-    println!("Final frequency: {}", current_frequency);
+    
+    // You can substantially simplify all of this with iterator adapters:
+    let freq = buffered.lines().map(|line| line.unwrap().parse::<i32>().unwrap()).sum();
+    println!("Final frequency: {}", freq);
+    
+    // Also, generally BufRead::lines is a bit of an antipattern, because it does way
+    // too much allocation (because it creates a new string for every line). This is a bit
+    // more idiomatic:
+    let mut input = String::new();
+    let f = File::open("inputs/01").unwrap();
+    f.read_to_string(&mut input).unwrap();
+    let freq = input.lines().map(|line| line.parse::<i32>().unwrap()).sum();
+    println!("Final frequency: {}", freq);
+    
 }
